66ebf904b68b5e3f854b9e1c9ee63080ca265880
cep104 <candicelizabethpeters@gmail.com>
2020-06-25 18:28:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
   taskForm.addEventListener('submit', function(e){
     e.preventDefault()
     const newTask = document.querySelector('#new-task-description').value;
-    const taskItem = document.createElement('li');
-    taskItem.innerText = newTask;
-    taskList.appendChild(taskItem);
+    // const taskItem = document.createElement('li');
+    // taskItem.innerText = newTask;
+    // taskList.appendChild(taskItem);
+    taskList.innerHTML += `
+    <li> ${newTask} 
+    <button data-action="delete"> X for delete </button>
+    </li>
+    `
+    taskForm.reset()
+  })
+  taskList.addEventListener('click', function(e){
+    console.log(e.target)
+    if (e.target.dataset.action === "delete"){
+      e.target.parentElement.remove();
+    }
   })
 });
