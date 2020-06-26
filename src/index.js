09c362a65aac26562930f338e2bd4526e96d8a1c
Sharon Watkins <swatkins876@gmail.com>
2020-06-26 13:42:47 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const taskForm = document.querySelector("#create-task-form")
+  // your code here
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value
+  taskList.innerHTML += `
+    <li>
+    ${newTask}
+    <button data-action="delete"> X </button>
+     </li>
+  `
+
+  taskForm.reset()
+});	});
+
+taskList.addEventListener("click", function(e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove()
+  }
+})
