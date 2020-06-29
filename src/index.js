cea2b3573155b3d2998b6ae97ad4c234034fa194
jamesabenj <james.abenj@gmail.com>
2020-06-29 17:59:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("input[type='submit']").addEventListener("click", function(){
+    event.preventDefault()
+    const list = document.getElementById('tasks')
+    let newListItem = document.createElement('li')
+    let task = document.getElementById('new-task-description').value
+    let button = document.createElement('button')
+    button.textContent = 'X'
+    button.setAttribute('data-description', task)
+    newListItem.innerText = task + " "
+    list.appendChild(newListItem)
+    newListItem.appendChild(button)
+
+    button.addEventListener('click', function(){
+      removeRow(button)
+    })
+    document.querySelector('form').reset()
+  })
 });
+
+function removeRow(e) {
+  e.parentNode.parentNode.removeChild(e.parentNode)
+}
+
+
+
+
