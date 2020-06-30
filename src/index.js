1a6150dd947ceb32e9e5fff122db1b3950f3b700
tnhubbar <taryn.johnson919@gmail.com>
2020-06-29 20:56:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const task = document.getElementById('new-task-description');
+
+  const newTaskUl = document.getElementById("tasks");
+
+  form.addEventListener('submit', addTask);
+
 });
+
+const newTask = event => {
+  event.preventDefault();
+  const task = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = task.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
