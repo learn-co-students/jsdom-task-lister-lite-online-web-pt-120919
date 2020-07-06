0ac95d46ebc77b597599c963283ccbeb25edf3c9
SMakaiTakori <makait87@icloud.com>
2020-07-05 22:43:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+  const tasks = document.getElementById("tasks")
+
+
+  taskForm.addEventListener("submit", function(e) {
+    event.preventDefault();
+     
+    //Grab the user input 
+    const newTask = document.getElementById("new-task-description").value
+
+    //Add newTask to DOM
+    tasks.innerHTML += `<li> ${newTask} <button data-action= "delete" style="color:red;"> X </button></li>`
+    
+    //resets the user input value
+    taskForm.reset()
+  })
+
+    tasks.addEventListener("click", function(e){
+        console.log(e.target)
+      if (e.target.dataset.action === "delete") {
+          e.target.parentElement.remove()
+      }
+    })
 });
