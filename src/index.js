ddf0231533126482e3ab422f6c8d30cf5d69d6d9
github username <github email address>
2020-07-08 16:10:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let id = document.querySelector("#new_task_description")
+  let taskForm = document.querySelector("#create-task-form")
+  console.log(taskForm)
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("hey")
+    let getDocument = document.getElementById("new-task-description").value
+    console.log(getDocument)
+let tasksUl = document.getElementById("tasks")
+console.log(tasksUl)
+    let li = document.createElement("li");
+    li.innerHTML = getDocument
+
+    tasksUl.appendChild(li)
+    console.log(event.target)
+    event.target.reset();
+
+  })
 });
+
+// everytime i submit something i should console log that something. get the data passing within the text Input, then display underneatht the ul id="ids"
