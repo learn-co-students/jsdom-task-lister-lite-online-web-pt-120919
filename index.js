697a94a6cb9ea0321e2ce7b00b6ac706ff738b35
gtblackmore <gtblackmore@gmail.com>
2020-08-31 15:58:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  
-  const taskList = new TaskList();
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const taskUl = document.getElementById("tasks");
-
-  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-  newTaskForm.addEventListener("submit", (e) => {
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-
-  taskUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
-});
+    const taskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = taskDescription.value;
+    document.getElementById("tasks").appendChild(newTask);
+    });
+  };
